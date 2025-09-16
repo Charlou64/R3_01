b2fa2443e88cea74374c8a5809d952547a4c3a24
Charlou64 <charlou060606@gmail.com>
2025-09-16 08:45:29 +0200

    exo1 final

--- exo1/exo1.js
+++ exo1/exo1.js
@@ -1,6 +1,9 @@
 let degreCelsuis = 20;
+console.log(degreCelsuis);//Cela affiche deg dans la console
 
 function CelcuisToFahrenheit(degCel){
     degCel = degCel *9/5+32;
+    console.log(degCel);
 }
 
+CelcuisToFahrenheit(degreCelsuis);
